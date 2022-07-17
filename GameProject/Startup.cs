5011bd16817c3b658bac2435bf5f26f5825f546a
Klym Tverdov <Klimtverdov@gmail.com>
2022-07-17 13:59:55 +0200

    fixup! reCreated whole project on Git

--- GameProject/Startup.cs
+++ GameProject/Startup.cs
@@ -13,16 +13,28 @@ namespace Game
             {
                 unit[0].Attack(unit[1]);
                 unit[1].Defence();
-                if (unit[1].HealthPoints <= 0)
+                unit[1].Attack(unit[0]);
+                unit[0].Defence();
+                if (unit[1].HealthPoints <= 0 || unit[0].HealthPoints <= 0)
                 {
                     Console.WriteLine("Killed");
                     break;
                 }
+                Console.WriteLine(unit[0].GetType() + " Unit 1 HP");
+                Console.WriteLine(Math.Round(unit[0].HealthPoints, 1));
+                Console.WriteLine(unit[1].GetType() + " Unit 2 HP");
                 Console.WriteLine(Math.Round(unit[1].HealthPoints, 1));
 
-            } while (unit[1].HealthPoints > 0);
-
-            Console.WriteLine(unit[0].ToString() + " killed " + unit[1].GetType());
+            } while (unit[1].HealthPoints > 0 || unit[0].HealthPoints > 0);
+            if (unit[0].HealthPoints < 0)
+            {
+                Console.WriteLine(unit[1].ToString() + " killed " + unit[0].GetType());
+            }
+            else
+            {
+                Console.WriteLine(unit[0].ToString() + " killed " + unit[1].GetType());
+            }
+            
 
         }
     }
